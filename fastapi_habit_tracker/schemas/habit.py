2a0b738c9056ab9d65b0da03e1e9b383868f0ec6
Filip <filip.wojszczak@gmail.com>
2025-12-04 19:57:39 +0100

    Add Pydantic schemas for Habit

--- fastapi_habit_tracker/schemas/habit.py
+++ fastapi_habit_tracker/schemas/habit.py
@@ -1,2 +1,21 @@
-class HabitCreate:
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+
+class HabitBase(BaseModel):
+    name: str = Field(min_length=1)
+    descrtiption: str | None = None
+    period: str
+
+
+class HabitCreate(HabitBase):
     pass
+
+
+class HabitRead(HabitBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
