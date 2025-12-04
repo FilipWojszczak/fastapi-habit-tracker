063e524827314d53e2b8ffd13509c5f4309f43b8
FilipWojszczak <62936153+FilipWojszczak@users.noreply.github.com>
2025-12-04 13:27:26 +0100

    Database setup (SQLModel models + init_db) (#2)
    * Add SQLModel models for User, Habit and HabitLog
    
    * Configure database engine and init_db
    
    * Fix created_at and updated_at fields in Habit model

--- fastapi_habit_tracker/models/habit_log.py
+++ fastapi_habit_tracker/models/habit_log.py
@@ -1,2 +1,13 @@
-class HabitLog:
-    pass
+from sqlmodel import SQLModel, Field, Relationship
+from datetime import datetime, timezone
+
+from .habit import Habit
+
+
+class HabitLog(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    habit_id: int = Field(foreign_key="habit.id")
+
+    timestamp: datetime = Field(default_factory=datetime.now(timezone.utc))
+
+    habit: "Habit" = Relationship(back_populates="logs")
