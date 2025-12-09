32277d4df4ab1f809317f2ae9e8aaea9eebafd8f
Filip <filip.wojszczak@gmail.com>
2025-12-09 18:10:06 +0100

    Add minimum length for password in UserCreate schema; set from_attributes flag in UserRead

--- fastapi_habit_tracker/schemas/user.py
+++ fastapi_habit_tracker/schemas/user.py
@@ -1,12 +1,15 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(min_length=8)
 
 
 class UserRead(BaseModel):
     id: int
     email: EmailStr
     is_active: bool
+
+    class Config:
+        from_attributes = True
