18525f6a309a96d771ca3381510260aca0cb4acc
Filip <filip.wojszczak@gmail.com>
2025-12-15 13:02:23 +0100

    Update HabitLog model

--- fastapi_habit_tracker/models/habit_log.py
+++ fastapi_habit_tracker/models/habit_log.py
@@ -11,6 +11,8 @@ class HabitLog(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     habit_id: int = Field(foreign_key="habit.id")
 
-    timestamp: datetime = Field(default_factory=lambda: datetime.now(UTC))
+    performed_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
+    note: str | None
+    duration: int | None
 
     habit: Habit = Relationship(back_populates="logs")
