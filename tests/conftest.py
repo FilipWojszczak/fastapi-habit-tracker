928dcd1e25ee9d7b4db493ae6e91aa8b228f8aa8
Filip <filip.wojszczak@gmail.com>
2026-01-22 22:54:29 +0100

    Modify session_fixture - use rollback
    Modification was made to clean db after each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,8 +21,17 @@ def session_fixture():
             "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
         )
     SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
+
+    connection = engine.connect()
+    transaction = connection.begin()
+
+    session = Session(bind=connection)
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
 
 
 @pytest.fixture(name="client")
