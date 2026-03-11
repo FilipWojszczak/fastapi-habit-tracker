3649e9a8512a034f7c69b0f010234ed43a6d792c
FilipWojszczak <62936153+FilipWojszczak@users.noreply.github.com>
2026-03-11 17:55:35 +0100

    Adjust unit tests to new a way of stats calculation

--- tests/unit/test_stats.py
+++ tests/unit/test_stats.py
@@ -1,11 +1,10 @@
 from datetime import UTC, datetime, timedelta
 
-from fastapi_habit_tracker.models import HabitLog
 from fastapi_habit_tracker.utils.stats import current_streak_days, longest_streak_days
 
 
-def create_log(days_ago: int):
-    return HabitLog(performed_at=datetime.now(UTC) - timedelta(days=days_ago))
+def create_date(days_ago: int):
+    return (datetime.now(UTC) - timedelta(days=days_ago)).date()
 
 
 def test_streak_empty_logs():
@@ -16,14 +15,14 @@ def test_streak_empty_logs():
 
 
 def test_current_streak_active_today():
-    logs = [create_log(0)]
+    logs = [create_date(0)]
 
     assert current_streak_days(logs) == 1
     assert longest_streak_days(logs) == 1
 
 
 def test_current_streak_broken_yesterday():
-    logs = [create_log(0), create_log(2)]
+    logs = [create_date(0), create_date(2)]
 
     assert current_streak_days(logs) == 1
     assert longest_streak_days(logs) == 1
@@ -31,11 +30,11 @@ def test_current_streak_broken_yesterday():
 
 def test_longest_streak_calculation():
     logs = [
-        create_log(0),
-        create_log(1),
-        create_log(4),
-        create_log(5),
-        create_log(6),
+        create_date(0),
+        create_date(1),
+        create_date(4),
+        create_date(5),
+        create_date(6),
     ]
 
     assert current_streak_days(logs) == 2
@@ -43,7 +42,7 @@ def test_longest_streak_calculation():
 
 
 def test_current_streak_is_zero_when_old_logs_only():
-    logs = [create_log(2), create_log(3)]
+    logs = [create_date(2), create_date(3)]
 
     assert current_streak_days(logs) == 0
     assert longest_streak_days(logs) == 2
